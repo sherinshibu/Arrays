d0bd234b4101cae9269c64aa32c5e9607016145b
sherinshibu <56407455+sherinshibu@users.noreply.github.com>
2019-11-21 19:06:46 +0000

    Update ArraysShoppingTask.js

--- ArraysShoppingTask.js
+++ ArraysShoppingTask.js
@@ -1,6 +1,10 @@
 var shoppingList = ["Eggs","Apples","Bread","Milk","Carrots","Potatoes","Chicken"] //the shopping list
 var fruitVegList = ["Apples","Apricot","Artichoke","Carrots","Cabbage","Onion","Oranges","Potatoes"] //the list of accepted fruits and vegetables
 
-var fruitVegShoppingList = [];
+var fruitVegShoppingList = ["Apples", "Carrots", "Potatoes"];
 
 // Code Below
+
+var shoppingList = ["Eggs","Apples","Bread","Milk","Carrots","Potatoes","Chicken"]
+var fruitVegList = shoppingList.indexOf("Apples","Apricot","Artichoke","Carrots","Cabbage","Onion","Oranges","Potatoes");
+document.getElementById("demo").innerHTML = "Apple is found in position " + b; "Carrots is found in position " + e; "Potatoes is found in position " + f;
